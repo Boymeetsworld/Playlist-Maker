884a3845301a8e0844638cfb21b0171eb7d936ec
Ayden Scott <aydenscott444@gmail.com>
2024-02-19 18:32:18 +0000

    Working on adding create playlist and add songs feature

--- menu.rb
+++ menu.rb
@@ -30,3 +30,35 @@ class Menu
         end
     end
 end
+
+def create_playlist
+    print "Enter playlist name: "
+    playlist_name = gets.chomp
+    playlist= Playlist.new(playlist_name)
+    @playlists > playlist
+    puts "Playlist '#{playlist_name}' was created"
+end
+
+def add_songs
+    
+    if @playlists.empty?
+    puts "No playlist is available, please create a playlist first"
+    return
+    end
+
+    puts "Select Playlist"
+    @playlist.each_with_index {|playlist, index| puts "#{index + 1}. #{playlist_name}"}
+    print "Enter playlist number: "
+    playlist_index = gets.chomp.to_i - 1
+
+    print "Enter song title here: "
+    song = gets.chomp
+    print "Enter album title here: "
+    album = gets.chomp
+    print "Enter artist here: "
+    artist = gets.chomp
+
+    song = Song.new(song,album,artist)
+    @playlist[playlist_index].add_songs(song)
+
+end
